bd990dd922255b062db8095ef449ed43c67baede
necabo <fynn.becker@gmx.de>
2021-08-15 03:24:23 +0200

    Use telescope find_files as fallback for git_files

--- lua/necabo/plugins/telescope/init.lua
+++ lua/necabo/plugins/telescope/init.lua
@@ -1,7 +1,10 @@
 local utils = require "necabo.utils"
 
--- TODO: make this fallback to `find_files` when not in a Git repository
-utils.map("n", "<leader>ff", "<cmd>Telescope git_files<cr>")
+utils.map(
+    "n", "<leader>ff",
+    "v:lua.necabo.is_git_repo() ? '<cmd>Telescope git_files<cr>' : '<cmd>Telescope find_files<cr>'",
+    {expr = true}
+)
 utils.map("n", "<leader>fa", "<cmd>Telescope find_files<cr>")
 utils.map("n", "<leader>frg", "<cmd>Telescope live_grep<cr>")
 utils.map("n", "<leader>fb", "<cmd>Telescope buffers<cr>")
--- lua/necabo/utils/init.lua
+++ lua/necabo/utils/init.lua
@@ -1,5 +1,15 @@
 local utils = {}
 
+local global = {}
+
+function global.is_git_repo()
+    local execute = vim.api.nvim_command
+    execute("silent! !git rev-parse --is-inside-work-tree")
+    return vim.v.shell_error == 0
+end
+
+_G["necabo"] = global
+
 utils.map = function(mode, lhs, rhs, opts)
     local options = {noremap = true}
     if opts then options = vim.tbl_extend("force", options, opts) end
