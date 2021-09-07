e523ff7e024bc8533825a874edd6ce87156125b1
necabo <fynn.becker@gmx.de>
2021-09-07 23:14:24 +0200

    Clean up lspconfig config
    - Move utils to their own submodule
    - Add clangd language server
    - Add custom Lua language server configuration

--- lua/necabo/plugins/lspconfig/init.lua
+++ lua/necabo/plugins/lspconfig/init.lua
@@ -1,48 +1,24 @@
-local utils = require "necabo.utils"
-
 local nvim_lsp = require "lspconfig"
 local lsp_status = require "lsp-status"
+local lspconfig_utils = require "necabo.plugins.lspconfig.utils"
 
-local on_attach = function(client, bufnr)
-    if client.resolved_capabilities.document_formatting then
-        utils.create_buffer_augroup(
-            {{[[BufWritePost <buffer> lua vim.lsp.buf.formatting_sync()]]}},
-            "necabo_format_on_save"
-        )
-    end
-
-    local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
 -- TODO: is this actually helpful or just straight up annoying?
 lsp_status.register_progress()
 
-    local opts = {noremap = true, silent = true }
+-- TODO: add keybindings to scroll in and close floating windows
 
-    buf_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-    buf_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-    buf_set_keymap("n", "gh", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-    buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-    buf_set_keymap("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-    buf_set_keymap("n", "<leader>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
-    buf_set_keymap("n", "<leader>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
-    buf_set_keymap("n", "<leader>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
-    buf_set_keymap("n", "<leader>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
-    buf_set_keymap("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-    buf_set_keymap("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
-    buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-    buf_set_keymap("n", "<leader>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", opts)
-    buf_set_keymap("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", opts)
-    buf_set_keymap("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", opts)
-    buf_set_keymap("n", "<leader>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
-    buf_set_keymap("n", "<leader>gf", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
-end
+vim.o.completeopt = "menuone,noselect"
 
 -- jdtls needs JDTLS_HOME set
-local servers = {"rust_analyzer", "jdtls"}
+local servers = {"rust_analyzer", "jdtls", "clangd"}
 for _, lsp in ipairs(servers) do
     nvim_lsp[lsp].setup {
-        on_attach = on_attach,
+        on_attach = lspconfig_utils.on_attach,
+        capabilities = lspconfig_utils.capabilities,
         flags = {
             debounce_text_changes = 150,
-        }
+        },
     }
 end
+
+require "necabo.plugins.lspconfig.lua"
