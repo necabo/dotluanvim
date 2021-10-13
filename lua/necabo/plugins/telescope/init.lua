4bbb1f756b272aa36ebaa85271ce9dc647b9aae1
necabo <fynn.becker@gmx.de>
2021-10-13 17:04:39 +0200

    Fix find_files cmd to actually find all files

--- lua/necabo/plugins/telescope/init.lua
+++ lua/necabo/plugins/telescope/init.lua
@@ -1,12 +1,14 @@
 local utils = require "necabo.utils"
 local telescope = require "telescope"
 
+local find_all_files_cmd = "<cmd>lua require(''telescope.builtin'').find_files({no_ignore = true, hidden = true})<cr>"
+
 utils.map(
     "n", "<leader>ff",
-    "v:lua.necabo.is_git_repo() ? '<cmd>Telescope git_files<cr>' : '<cmd>Telescope find_files<cr>'",
+    "v:lua.necabo.is_git_repo() ? '<cmd>Telescope git_files<cr>' : '" .. find_all_files_cmd .. "'",
     {expr = true}
 )
-utils.map("n", "<leader>fa", "<cmd>Telescope find_files<cr>")
+utils.map("n", "<leader>fa", find_all_files_cmd)
 utils.map("n", "<leader>frg", "<cmd>Telescope live_grep<cr>")
 utils.map("n", "<leader>fb", "<cmd>Telescope buffers<cr>")
 utils.map("n", "<leader>fh", "<cmd>Telescope help_tags<cr>")
