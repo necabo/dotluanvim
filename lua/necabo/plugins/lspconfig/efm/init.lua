2baded2a7c1b1d347d8b657b425ad235dec5dbe0
necabo <fynn.becker@gmx.de>
2021-10-23 05:52:27 +0200

    Configure lua linter and autoformatter

--- lua/necabo/plugins/lspconfig/efm/init.lua
+++ lua/necabo/plugins/lspconfig/efm/init.lua
@@ -4,12 +4,16 @@ local black = require "necabo.plugins.lspconfig.efm.black"
 local isort = require "necabo.plugins.lspconfig.efm.isort"
 local flake8 = require "necabo.plugins.lspconfig.efm.flake8"
 
+local luacheck = require "necabo.plugins.lspconfig.efm.luacheck"
+local stylua = require "necabo.plugins.lspconfig.efm.stylua"
+
 require("lspconfig").efm.setup {
-    filetypes = { "python" },
+    filetypes = { "python", "lua" },
     settings = {
         rootMarkers = { ".git/"},
         languages = {
             python = { black, isort, flake8 },
+            lua = { luacheck, stylua },
         },
     },
     init_options = {
---
+++ lua/necabo/plugins/lspconfig/efm/luacheck.lua
@@ -0,0 +1,7 @@
+return {
+    lintCommand = "luacheck --formatter plain -",
+    lintStdin = true,
+    lintIgnoreExitCode = true,
+    lintFormats = { "%f:%l:%c: %m" },
+    lintSource = "luacheck",
+}
