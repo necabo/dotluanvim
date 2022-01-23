d1bbdfbb35c1ca17e39f7e6a7118d611385c3c74
necabo <fynn.becker@gmx.de>
2022-01-23 15:02:41 +0100

    Update gitsigns config

--- lua/necabo/plugins/gitsigns/init.lua
+++ lua/necabo/plugins/gitsigns/init.lua
@@ -1,23 +1,34 @@
 vim.opt.signcolumn = "yes"
 
 require("gitsigns").setup({
-  keymaps = {
-    noremap = true,
+  on_attach = function(bufnr)
+    local gs = package.loaded.gitsigns
 
-    ["n ]h"] = '<cmd>lua require"gitsigns.actions".next_hunk()<CR>',
-    ["n [h"] = '<cmd>lua require"gitsigns.actions".prev_hunk()<CR>',
+    local function map(mode, l, r, opts)
+      opts = opts or {}
+      opts.buffer = bufnr
+      vim.keymap.set(mode, l, r, opts)
+    end
 
-    ["n <leader>hs"] = '<cmd>lua require("gitsigns").stage_hunk()<CR>',
-    ["v <leader>hs"] = '<cmd>lua require("gitsigns").stage_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
-    ["n <leader>hu"] = '<cmd>lua require("gitsigns").undo_stage_hunk()<CR>',
-    ["n <leader>hr"] = '<cmd>lua require("gitsigns").reset_hunk()<CR>',
-    ["v <leader>hr"] = '<cmd>lua require("gitsigns").reset_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
-    ["n <leader>hR"] = '<cmd>lua require("gitsigns").reset_buffer()<CR>',
-    ["n <leader>hp"] = '<cmd>lua require("gitsigns").preview_hunk()<CR>',
-    ["n <leader>hb"] = '<cmd>lua require("gitsigns").blame_line(true)<CR>',
+    map("n", "]h", gs.next_hunk)
+    map("n", "[h", gs.prev_hunk)
+
+    map("n", "<leader>hs", gs.stage_hunk)
+    map("v", "<leader>hs", function()
+      gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+    end)
+    map("n", "<leader>hu", gs.undo_stage_hunk)
+    map("n", "<leader>hr", gs.reset_hunk)
+    map("v", "<leader>hr", function()
+      gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+    end)
+    map("n", "<leader>hR", gs.reset_buffer)
+    map("n", "<leader>hp", gs.preview_hunk)
+    map("n", "<leader>hb", function()
+      gs.blame_line({ full = true })
+    end)
 
     -- Text objects
-    ["o ih"] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>',
-    ["x ih"] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>',
-  },
+    map({ "o", "x" }, "ih", ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>')
+  end,
 })
